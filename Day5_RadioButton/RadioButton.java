0334876d890c02c146575dfc4d1b46fc65831976
MANOJKUMAR <101208123+Manojkumar4636@users.noreply.github.com>
2022-04-28 19:54:39 +0530

    Initial Commit

---
+++ Day5_RadioButton/RadioButton.java
@@ -0,0 +1,45 @@
+package com.learn.selenium.day5;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/*
+TestCase1: Open Chrome Browser.
+TestCase2: Visit the WebSite: "http://www.leafground.com/pages/radio.html"
+TestCase3: Click YES or NO button
+TestCase4: Find default selected radio button
+TestCase5: Select your age group
+TestCase6: Close the project
+*/
+public class RadioButtonExample {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.leafground.com/pages/radio.html");
+		
+		//Click YES or NO button
+		WebElement clickYes = driver.findElement(By.id("yes"));
+		clickYes.click();
+		
+		//Find default selected radio button
+		WebElement findSelectButton = driver.findElement(By.xpath("//input[@name=\"news\"][@value=\"0\"]"));
+		boolean uncheckedButton = findSelectButton.isSelected();
+		System.out.println("Unchecked: "+uncheckedButton);
+		
+		WebElement findSelectButton1 = driver.findElement(By.xpath("//input[@name=\"news\"][@value=\"1\"]"));
+		boolean checkedButton = findSelectButton1.isSelected();
+		System.out.println("Checked: "+checkedButton);
+		
+		//Select your age group
+		WebElement findSelectButton2 = driver.findElement(By.xpath("//input[@name=\"age\"][@value=\"0\"]"));
+		findSelectButton2.click();
+		
+		//Close the project
+		driver.quit();
+		
+	}
+}
