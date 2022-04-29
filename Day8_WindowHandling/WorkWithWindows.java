90872117c518d5efdfe233bb27f7270c416c4533
MANOJKUMAR <101208123+Manojkumar4636@users.noreply.github.com>
2022-04-29 18:58:34 +0530

    Initial Commit

---
+++ Day8_WindowHandling/WorkWithWindows.java
@@ -0,0 +1,84 @@
+package com.learn.selenium.day7;
+
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WorkWithWindowsExample {
+	/*
+	TestCase1: Open Chrome Browser.
+	TestCase2: Visit the WebSite: "http://www.leafground.com/pages/Window.html"
+	TestCase3: Click button to open home page in New Window
+    TestCase4: Find the number of opened windows
+    TestCase5: Close all windows except Parent Windows
+    TestCase6: Wait for 2 new Windows to open
+    TestCase7: Close the project
+	 */
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.leafground.com/pages/Window.html");
+
+		//Parent window identity store it
+		String oldWindow = driver.getWindowHandle();
+
+		//Click button to open home page in New Window
+		WebElement firstButton = driver.findElement(By.id("home"));
+		firstButton.click();
+
+		//All window identity store it
+		Set<String> windowHandles = driver.getWindowHandles();
+
+		for (String newWindow : windowHandles) {
+			driver.switchTo().window(newWindow);
+		}
+		WebElement editBox = driver.findElement(By.xpath("//*[@id=\"post-153\"]/div[2]/div/ul/li[1]/a"));
+		editBox.click();
+
+		driver.close();
+
+		driver.switchTo().window(oldWindow);
+
+		//Find the number of opened windows
+		WebElement openMultiple = driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[2]/div/div/button"));
+		openMultiple.click();
+
+		int numberOfWindows = driver.getWindowHandles().size();
+		System.out.println("NumberOfWindows: "+numberOfWindows);
+
+		//Close all windows except Parent Windows
+		WebElement dontCloseMe = driver.findElement(By.id("color"));
+		dontCloseMe.click();
+
+		Set<String> windowHandles2 = driver.getWindowHandles();
+
+		for (String allWindows : windowHandles2) {
+			if (!allWindows.equals(oldWindow)) {
+				driver.switchTo().window(allWindows);
+				driver.close();
+			}
+		}
+
+		driver.switchTo().window(oldWindow);
+		
+		//Wait for 2 new Windows to open
+		WebElement LastWindow = driver.findElement(By.xpath("//button[@id=\"color\"][@style=\"background-color:lightblue\"]"));
+		LastWindow.click();
+
+		Set<String> windowHandles3 = driver.getWindowHandles();
+		for (String allWindows : windowHandles3) {
+			if (!allWindows.equals(oldWindow)) {
+				driver.switchTo().window(allWindows);
+				driver.close();	
+			}
+		}
+		
+		//Close the project
+		driver.quit();
+	}
+
+}
