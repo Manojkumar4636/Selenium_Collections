512be8b771a2e4acc72c3b553c0e8f7d5b52d363
MANOJKUMAR <101208123+Manojkumar4636@users.noreply.github.com>
2022-05-01 19:59:55 +0530

    Initial Commit

--- Day13_AutoComplete/AutoComplete.java
+++ Day13_AutoComplete/AutoComplete.java
@@ -11,19 +11,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AutoCompleteExample {
     /*
 	TestCase1: Open Chrome Browser.
-	TestCase2: Visit the WebSite: "http://www.leafground.com/pages/autoComplete.html"
+	TestCase2: Visit the WebSite: "https://manojkumar4636.github.io/Selenium_Practice_Hub/pages/autoComplete.html"
 	TestCase3: Search the some word Example i'm search "S"
 	TestCase4: Find List Size
 	TestCase5: Select the value from the suggestion.
-	TestCase6: Close the project.
+	TestCase6: Quit the project.
 	*/
 	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "ENTER YOUR CHROMEDRIVER PATH");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
-		driver.get("http://www.leafground.com/pages/autoComplete.html");
+		driver.get("https://manojkumar4636.github.io/Selenium_Practice_Hub/pages/autoComplete.html");
 		
 		//Search the some word Example i'm search "S"
 		WebElement input = driver.findElement(By.id("tags"));
@@ -42,7 +42,7 @@ public class AutoCompleteExample {
 			}
 		}
 	    
-	    //Close the project
+	    //Quit the project
 	    driver.quit();
 	}
 
