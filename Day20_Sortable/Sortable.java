67e517e23c824d8bc576b95286d43c51a359f526
Manojkumar Murugesan <101208123+Manojkumar4636@users.noreply.github.com>
2022-05-05 10:16:00 +0530

    Initial Commit

---
+++ Day20_Sortable/Sortable.java
@@ -0,0 +1,37 @@
+package com.learn.selenium.day20;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class SortableExample {
+	/*
+	TestCase1: Open Chrome Browser.
+	TestCase2: Visit the WebSite: "http://www.leafground.com/pages/sortable.html"
+	TestCase3: Move the last index value from first index
+	TestCase4: Close the project
+	*/
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("http://www.leafground.com/pages/sortable.html");
+
+		//Move the last index value from first index
+		List<WebElement> elements = driver.findElements(By.xpath("//*[@id=\"sortable\"]/li"));
+		WebElement from = elements.get(6);
+		WebElement to = elements.get(0);
+		
+		Actions action = new Actions(driver);
+		action.dragAndDrop(from, to).build().perform();
+		
+		//Close the project
+		driver.quit();
+		
+	}
+
+}
