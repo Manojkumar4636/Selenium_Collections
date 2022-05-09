327cb147e19be5956726ecd31c9e602be89888a4
Manojkumar Murugesan <101208123+Manojkumar4636@users.noreply.github.com>
2022-05-09 20:03:49 +0530

    Initial Commit

---
+++ INTERVIEW_QUESTIONS/GetAndNavigate/GetAndNavigate.java
@@ -0,0 +1,39 @@
+package com.learn.selenium.totalExamples;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GetAndNavgateExample {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		//get will not store the history
+		//driver.get("http://www.google.co.in"); //Waiting for the elements appear in DOM
+		
+		//navigate will store the history
+		driver.navigate().to("http://www.google.co.in");
+		
+		WebElement search = driver.findElement(By.name("q"));
+		search.sendKeys("Goa Trip"+Keys.ENTER);
+		
+		Thread.sleep(2000);
+		driver.navigate().back();
+		
+		Thread.sleep(2000);
+		driver.navigate().forward();
+		
+		Thread.sleep(2000);
+		driver.navigate().refresh();
+		
+		//Close the project
+		driver.quit();
+
+	}
+
+}
