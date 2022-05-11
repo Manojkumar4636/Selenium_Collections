8ef3dc45c93e193ef2baa8f42ec9c593e15df92e
Manojkumar Murugesan <101208123+Manojkumar4636@users.noreply.github.com>
2022-05-11 13:55:19 +0530

    Initial Commit

---
+++ INTERVIEW_QUESTIONS/MaximizedBrowser/MaximizedBrowser.java
@@ -0,0 +1,36 @@
+package com.learn.selenium.totalExamples;
+
+
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class MaxmizeExample {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+		/*
+		1st Method 
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("start-maximized");
+		*/
+		WebDriver driver = new ChromeDriver(); // ( ) --> Passing the chromeOptions
+		driver.get("http://www.google.co.in");
+		
+		//2nd Method
+		driver.manage().window().maximize();
+		
+		/*
+		3rd Method
+		Dimension dimension = new Dimension(1366, 768);
+		driver.manage().window().setSize(dimension);
+		*/
+		
+	
+	
+	}
+
+}
