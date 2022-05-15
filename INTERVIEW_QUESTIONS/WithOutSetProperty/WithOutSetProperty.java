050227ceae4887c1e1714bd5e4f9267f3fda65f9
Manojkumar Murugesan <101208123+Manojkumar4636@users.noreply.github.com>
2022-05-15 15:24:02 +0530

    Initial Commit

--- INTERVIEW_QUESTIONS/WithOutSetProperty/WithOutSetProperty.java
+++ INTERVIEW_QUESTIONS/WithOutSetProperty/WithOutSetProperty.java
@@ -7,6 +7,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WithOutSetProperty {
 
+	/*
+	1) First of click the " Window-Start " button
+        2) Then Search " Edit the system environment variables " and click 
+        3) After click one new window will be open and click " environment variables " Button
+        4) And another " Environment variables " window is opened, you go for " System Variables " and 
+           " PATH " is click.
+
+        5) Then your ChromeDriver path is Copy and Paste the " System Variables PATH " after 
+        click ok button the  all opened windows.
+	*/
 	
 	public static void main(String[] args) {
 		
