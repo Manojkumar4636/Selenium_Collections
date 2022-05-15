9cb25075a78ef4d25be99480d78b0c25e5ad6fd1
Manojkumar Murugesan <101208123+Manojkumar4636@users.noreply.github.com>
2022-05-15 15:05:32 +0530

    Initial Commit

---
+++ INTERVIEW_QUESTIONS/WithOutSetProperty/WithOutSetProperty.java
@@ -0,0 +1,24 @@
+package com.learn.selenium.totalExamples;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WithOutSetProperty {
+
+	public static void main(String[] args) {
+		
+		//With out SetProperty use to launch the Browser
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.co.in");
+		
+		WebElement search = driver.findElement(By.name("q"));
+		search.sendKeys("Goa Trip \n");
+		
+		WebElement clickLink = driver.findElement(By.xpath("//h3[(text()='84 Goa Tour Packages - MakeMyTrip')]"));
+		clickLink.click();
+	}
+
+}
+
